9ac8ee5883ed329533a0d02649a84e293ad8afe4
zer0ken <a37592777@gmail.com>
2024-04-09 21:27:43 +0900

    ClientsWindow 추가

--- src/main/java/org/example/components/ClientsWindow.java
+++ src/main/java/org/example/components/ClientsWindow.java
@@ -1,9 +1,36 @@
 package org.example.components;
 
+import javax.swing.*;
+
+import java.awt.*;
+
 import static org.example.components._Constants.*;
 
 public class ClientsWindow extends _ComponentJPanel {
+    DefaultListModel<String> clientListModel;
+    JList<String> clientList;
+
     ClientsWindow() {
         super(CLIENTS_WINDOW_SIZE);
+        setBorder(BorderFactory.createTitledBorder(CLIENTS_WINDOW_TITLE));
+        setLayout(new BorderLayout());
+
+        clientListModel = new DefaultListModel<>();
+
+        for (int i = 0; i < 100; i++) {
+            clientListModel.addElement("tester "+i);
+        }
+
+        clientList = new JList<>(clientListModel);
+
+        JScrollPane scrollPane = new JScrollPane(
+                clientList,
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_NEVER
+        );
+
+        scrollPane.setPreferredSize(new Dimension(200, 300));
+
+        add(BorderLayout.CENTER, scrollPane);
     }
 }
--- src/main/java/org/example/components/_Constants.java
+++ src/main/java/org/example/components/_Constants.java
@@ -1,5 +1,6 @@
 package org.example.components;
 
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class _Constants {
@@ -10,12 +11,21 @@ public class _Constants {
     public static final String APP_TITLE = "앱 이름이 여기에 들어가야 한단다~";
     public static final String LOGIN_TITLE = "로그인";
     public static final String LOGIN_MESSAGE = "다른 사용자에게 표시할 이름을 입력하세요.";
+    public static final String[] STYLE_LABELS = {
+            "획 두께(px)", "획 색상", "채우기 색상", "텍스트 크기(px)", "텍스트 색상", "텍스트 내용" };
+    public static final String STYLE_WINDOW_TITLE = "스타일 지정";
+    public static final String STYLE_LINE_COLOR_TITLE = "획 색상을 선택하세요.";
+    public static final String STYLE_FILL_COLOR_TITLE = "채우기 색상을 선택하세요.";
+    public static final String STYLE_TEXT_COLOR_TITLE = "텍스트 색상을 선택하세요.";
+    public static final String STYLE_DEFAULT_TEXT_CONTENT = "Text Content";
+    public static final String CLIENTS_WINDOW_TITLE = "동시 접속자 명단";
+
 
     // size
     public static final int APP_WIDTH = 1400;
     public static final int APP_HEIGHT = 900;
     public static final int APP_LEFT_WIDTH = 200;
-    public static final int APP_RIGHT_WIDTH = 300;
+    public static final int APP_RIGHT_WIDTH = 200;
     public static final int APP_CENTER_WIDTH = APP_WIDTH - APP_LEFT_WIDTH - APP_RIGHT_WIDTH;
 
     public static final int STYLE_WINDOW_HEIGHT = 300;
@@ -28,5 +38,15 @@ public class _Constants {
     public static final Dimension CANVAS_SIZE = new Dimension(APP_CENTER_WIDTH, APP_HEIGHT - TOOLBAR_HEIGHT);
     public static final Dimension CLIENTS_WINDOW_SIZE = new Dimension(APP_RIGHT_WIDTH, CLIENTS_WINDOW_HEIGHT);
     public static final Dimension SHAPES_WINDOW_SIZE =new Dimension(APP_RIGHT_WIDTH, APP_HEIGHT - CLIENTS_WINDOW_HEIGHT);
+
+
+    public static final int V_SPACE = 4;
+    public static final int H_SPACE = 8;
+
+    public static final int STYLE_ITEM_HEIGHT = 26;
+    public static final int STYLE_SLOT_WIDTH = 64;
+    public static final EmptyBorder STYLE_ITEM_BORDER = new EmptyBorder(V_SPACE, H_SPACE, V_SPACE, H_SPACE);
+    public static final Dimension STYLE_ITEM_SIZE = new Dimension(APP_LEFT_WIDTH, STYLE_ITEM_HEIGHT);
+    public static final Dimension STYLE_LARGE_ITEM_SIZE = new Dimension(APP_LEFT_WIDTH, STYLE_ITEM_HEIGHT*2);
 
 }
