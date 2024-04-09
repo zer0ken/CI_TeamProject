2a5e9dbdb4aad7279174127963757b9ff86bb9cc
zer0ken <a37592777@gmail.com>
2024-04-09 14:56:40 +0900

    상수 수정

--- src/main/java/org/example/components/_Constants.java
+++ src/main/java/org/example/components/_Constants.java
@@ -14,23 +14,25 @@ public class _Constants {
     // size
     public static final int APP_WIDTH = 1400;
     public static final int APP_HEIGHT = 900;
-    public static final int APP_SIDE_WIDTH = 200;
-    public static final int APP_CENTER_WIDTH = APP_WIDTH - 2 * APP_SIDE_WIDTH;
+    public static final int APP_LEFT_WIDTH = 200;
+    public static final int APP_RIGHT_WIDTH = 300;
+    public static final int APP_CENTER_WIDTH = APP_WIDTH - APP_LEFT_WIDTH - APP_RIGHT_WIDTH;
 
     public static final int STYLE_WINDOW_HEIGHT = 300;
     public static final int TOOLBAR_HEIGHT = 30;
     public static final int CLIENTS_WINDOW_HEIGHT = 300;
+
     public static final Rectangle STYLE_WINDOW_BOUNDS =
-            new Rectangle(0, 0, APP_SIDE_WIDTH, STYLE_WINDOW_HEIGHT);
+            new Rectangle(0, 0, APP_LEFT_WIDTH, STYLE_WINDOW_HEIGHT);
     public static final Rectangle EDIT_WINDOW_BOUNDS =
-            new Rectangle(0, STYLE_WINDOW_HEIGHT, APP_SIDE_WIDTH, APP_HEIGHT - STYLE_WINDOW_HEIGHT);
+            new Rectangle(0, STYLE_WINDOW_HEIGHT, APP_LEFT_WIDTH, APP_HEIGHT - STYLE_WINDOW_HEIGHT);
     public static final Rectangle TOOLBAR_BOUNDS =
-            new Rectangle(APP_SIDE_WIDTH, 0, APP_CENTER_WIDTH, TOOLBAR_HEIGHT);
+            new Rectangle(APP_LEFT_WIDTH, 0, APP_CENTER_WIDTH, TOOLBAR_HEIGHT);
     public static final Rectangle CANVAS_BOUNDS =
-            new Rectangle(APP_SIDE_WIDTH, TOOLBAR_HEIGHT, APP_CENTER_WIDTH, APP_HEIGHT - TOOLBAR_HEIGHT);
+            new Rectangle(APP_LEFT_WIDTH, TOOLBAR_HEIGHT, APP_CENTER_WIDTH, APP_HEIGHT - TOOLBAR_HEIGHT);
     public static final Rectangle CLIENTS_WINDOW_BOUNDS =
-            new Rectangle(APP_SIDE_WIDTH + APP_CENTER_WIDTH, 0, APP_SIDE_WIDTH, CLIENTS_WINDOW_HEIGHT);
+            new Rectangle(APP_LEFT_WIDTH + APP_CENTER_WIDTH, 0, APP_RIGHT_WIDTH, CLIENTS_WINDOW_HEIGHT);
     public static final Rectangle SHAPES_WINDOW_BOUNDS =
-            new Rectangle(APP_SIDE_WIDTH + APP_CENTER_WIDTH, CLIENTS_WINDOW_HEIGHT,
-                    APP_SIDE_WIDTH, APP_HEIGHT - CLIENTS_WINDOW_HEIGHT);
+            new Rectangle(APP_LEFT_WIDTH + APP_CENTER_WIDTH, CLIENTS_WINDOW_HEIGHT,
+                    APP_RIGHT_WIDTH, APP_HEIGHT - CLIENTS_WINDOW_HEIGHT);
 }
