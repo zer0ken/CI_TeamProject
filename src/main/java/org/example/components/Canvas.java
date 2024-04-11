77b1903f0f71daacab63f19f961d5104b51efc4b
tk lee <ltk990328@gmail.com>
2024-04-11 22:02:44 +0900

    도형 타입을 인자로 받는 메소드 추가

--- src/main/java/org/example/components/Canvas.java
+++ src/main/java/org/example/components/Canvas.java
@@ -1,13 +1,48 @@
 package org.example.components;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import static org.example.components._Constants.CANVAS_SIZE;
 
 public class Canvas extends _ComponentJPanel {
+
+    private ArrayList<Shape> shapes; // shape list
     Canvas() {
         super(CANVAS_SIZE);
         setLayout(new BorderLayout());
         setBackground(Color.white);
+        shapes = new ArrayList<>();
+    }
 
+    // draw shape
+    public void drawShape(Shape shape) {
+        shapes.add(shape);
+        repaint();
+    }
+
+    // modify shape
+    public void modifyShape(int index, Shape modifiedShape) {
+        if (index >= 0 && index < shapes.size()) {
+            shapes.set(index, modifiedShape);
+            repaint();
+        }
+    }
+
+    // delete shape
+    public void deleteShape(int index) {
+        if (index >= 0 && index < shapes.size()) {
+            shapes.remove(index);
+            repaint(); // 컴포넌트를 다시 그리도록 요청
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // paint all stored shape
+//        for (Shape shape : shapes) {
+//            shape.draw(g);
+//        }
     }
 }
