36e983571db4180cc689eacf68a1c3ce64eab886
tk lee <ltk990328@gmail.com>
2024-04-11 21:04:03 +0900

    Canvas 기본 흰 화면 추가

--- src/main/java/org/example/components/Canvas.java
+++ src/main/java/org/example/components/Canvas.java
@@ -1,9 +1,13 @@
 package org.example.components;
 
+import java.awt.*;
 import static org.example.components._Constants.CANVAS_SIZE;
 
 public class Canvas extends _ComponentJPanel {
     Canvas() {
         super(CANVAS_SIZE);
+        setLayout(new BorderLayout());
+        setBackground(Color.white);
+
     }
 }
