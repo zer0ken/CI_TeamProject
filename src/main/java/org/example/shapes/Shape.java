73c325e376a10927a74e412eea28a01e86002a8f
tk lee <ltk990328@gmail.com>
2024-04-15 21:06:36 +0900

    shapes 패키지에 도형 클래스 정의

---
+++ src/main/java/org/example/shapes/Shape.java
@@ -0,0 +1,33 @@
+package org.example.shapes;
+
+import java.awt.*;
+import java.util.Date;
+
+public class Shape {
+  private long id;
+  private Point startPoint;
+  private Style style;
+
+  public Shape(Point startPoint, Style style) {
+    this.id = new Date().getTime();
+    this.startPoint = startPoint;
+    this.style = style;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId() {
+    this.id = new Date().getTime();
+  }
+
+  public Point getStartPoint() {
+    return startPoint;
+  }
+
+  public void setStartPoint(Point startPoint) {
+    this.startPoint = startPoint;
+  }
+
+}
