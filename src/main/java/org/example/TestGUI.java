37b70fbd711b9a672c73a1a713df7dd9d06988a6
zer0ken <a37592777@gmail.com>
2024-04-16 11:40:08 +0900

    Main은 더이상 Main이 아니다

--- src/main/java/org/example/TestGUI.java
+++ src/main/java/org/example/TestGUI.java
@@ -3,7 +3,7 @@ package org.example;
 import org.example.components.App;
 import org.example.components.Login;
 
-public class Main {
+public class TestGUI {
     public static void main(String[] args) {
         System.out.println(Login.login());
         new App();
