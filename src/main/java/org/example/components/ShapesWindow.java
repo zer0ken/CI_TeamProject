7b4e84e13f9bc9a4303405e0cb1a552667c1bf4d
Seori-Lee <49297010+Seori-Lee@users.noreply.github.com>
2024-04-17 21:24:20 +0900

    Update ShapesWindow.java

--- src/main/java/org/example/components/ShapesWindow.java
+++ src/main/java/org/example/components/ShapesWindow.java
@@ -1,9 +1,28 @@
 package org.example.components;
 
-import static org.example.components._Constants.SHAPES_WINDOW_SIZE;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static org.example.components._Constants.*;
 
 public class ShapesWindow extends _ComponentJPanel {
+
+    JList<String> ShapeEditList;
+    
     ShapesWindow() {
         super(SHAPES_WINDOW_SIZE);
+
+        JPanel ShapeEditPanel = new JPanel(new BorderLayout());
+        ShapeEditPanel.setLayout(new BoxLayout(ShapeEditPanel, BoxLayout.Y_AXIS));
+        ShapeEditPanel.add(new JScrollPane(ShapeEditList), BorderLayout.CENTER);
+
+        // 1) Canvas상에서 도형을 클릭했을 경우
+
+        ShapeEditList.addElement(); // 해당 도형의 ID 추가
+
+        // 2) 도형을 편집할 경우
+        // -> Style
+        // 기존 도형을 수정해서 Canvas에 새로 그리고 이전 도형은 삭제
+        
     }
 }
