7eb70003c2a66bfa5ce0a159764c76b458d40278
Seori-Lee <49297010+Seori-Lee@users.noreply.github.com>
2024-04-17 22:01:33 +0900

    Update EditWindow.java

--- src/main/java/org/example/components/EditWindow.java
+++ src/main/java/org/example/components/EditWindow.java
@@ -1,9 +1,30 @@
 package org.example.components;
 
-import static org.example.components._Constants.EDIT_WINDOW_SIZE;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import static org.example.components._Constants.*;
 
-public class EditWindow extends _ComponentJPanel {
-    EditWindow() {
-        super(EDIT_WINDOW_SIZE);
+public class ShapesWindow extends _ComponentJPanel {
+
+    JList<String> ShapeEditList;
+    
+    ShapesWindow() {
+        super(SHAPES_WINDOW_SIZE);
+
+        ShapeEditList.removeAllElements();
+
+        JPanel ShapeEditPanel = new JPanel(new BorderLayout());
+        ShapeEditPanel.setLayout(new BoxLayout(ShapeEditPanel, BoxLayout.Y_AXIS));
+        ShapeEditPanel.add(new JScrollPane(ShapeEditList), BorderLayout.CENTER);
+
+        // 1) Canvas상에서 도형을 클릭했을 경우
+
+        ShapeEditList.addElement(); // 서버에서 해당 도형의 ID를 받아온 후 리스트에 추가
+
+        // 2) 도형을 편집할 경우
+        // -> Style
+        // 기존 도형을 수정해서 Canvas에 새로 그리고 이전 도형은 삭제
+        
     }
 }
