4046d379e090d2b21846539071761b9f23621991
zer0ken <a37592777@gmail.com>
2024-04-26 10:46:15 +0900

    뷰모델 기반으로 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ import org.example.components.Login;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Login.login());
-        new App();
+        new App(new ShapesViewModel());
     }
 }
