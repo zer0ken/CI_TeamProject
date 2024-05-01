47683d7055d0989c00082d1480e8cc60cee1af3e
zer0ken <a37592777@gmail.com>
2024-05-02 02:49:43 +0900

    Toolbar 모델 분리

--- src/main/java/org/client/gui/components/Toolbar.java
+++ src/main/java/org/client/gui/components/Toolbar.java
@@ -1,20 +1,13 @@
 package org.client.gui.components;
 
-import org.client.gui.shapes.Line;
-import org.client.gui.shapes.Oval;
-import org.client.gui.shapes.Rectangle;
-import org.client.gui.shapes.Text;
+import org.client.gui.models.ToolbarMouseAdapter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import static org.client.gui.Constants.*;
 
 public class Toolbar extends ComponentJPanel {
-    private long count = 0;                 // 도형 id용(임시)
-
     public Toolbar() {
         super(TOOLBAR_SIZE);
         setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -27,32 +20,8 @@ public class Toolbar extends ComponentJPanel {
         for (String toolbarButton : TOOLBAR_BUTTONS) {
             JButton button = new JButton(toolbarButton);
             button.setFocusPainted(false);
-            button.addMouseListener(new DoubleClickListener());
+            button.addMouseListener(new ToolbarMouseAdapter());
             add(button);
-        }
-
-    }
-
-    private class DoubleClickListener extends MouseAdapter {
-        public void mouseClicked(MouseEvent e) {
-            if (e.getClickCount() == 2) {
-                JButton source = (JButton) e.getSource();
-                org.client.gui.shapes.Shape clickedShape;
-                switch (source.getText()) {
-                    case TOOLBAR_LINE -> clickedShape = new Line();
-                    case TOOLBAR_RECT -> clickedShape = new Rectangle();
-                    case TOOLBAR_OVAL -> clickedShape = new Oval();
-                    case TOOLBAR_TEXT -> clickedShape = new Text();
-                    default -> {
-                        return;
-                    }
-                }
-                clickedShape.setLocation(300, 300, 400, 400);
-                clickedShape.setStyle(appModel.getStyle());
-                count++;
-                clickedShape.setId(count);
-                appModel.createByUser(clickedShape);
-            }
         }
     }
 }
---
+++ src/main/java/org/client/gui/models/ToolbarMouseAdapter.java
@@ -0,0 +1,35 @@
+package org.client.gui.models;
+
+import org.client.gui.shapes.*;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import static org.client.gui.Constants.*;
+
+public class ToolbarMouseAdapter extends MouseAdapter {
+    private final AppModel appModel = AppModel.getInstance();
+    private long count = 0;                 // 도형 id용(임시)
+
+    public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() == 2) {
+            JButton source = (JButton) e.getSource();
+            Shape clickedShape;
+            switch (source.getText()) {
+                case TOOLBAR_LINE -> clickedShape = new Line();
+                case TOOLBAR_RECT -> clickedShape = new Rectangle();
+                case TOOLBAR_OVAL -> clickedShape = new Oval();
+                case TOOLBAR_TEXT -> clickedShape = new Text();
+                default -> {
+                    return;
+                }
+            }
+            clickedShape.setLocation(300, 300, 400, 400);
+            clickedShape.setStyle(appModel.getStyle());
+            clickedShape.setId(count++);
+            appModel.createByUser(clickedShape);
+        }
+    }
+}
+
